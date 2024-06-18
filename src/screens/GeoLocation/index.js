5a2635ca146dd794e6786de1596da6a88d9f9796
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-06-18 12:47:55 +0530

    geolocation started

---
+++ src/screens/GeoLocation/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function GeoLoaction() {
+  return (
+    <View>
+      <Text>GeoLoaction</Text>
+    </View>
+  )
+}
