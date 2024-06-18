80cdff6615d127b8486ae739f2441e606081b94d
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-06-18 18:09:13 +0530

    getlocation and openMaps

--- src/screens/GeoLocation/index.tsx
+++ src/screens/GeoLocation/index.tsx
@@ -5,6 +5,8 @@ import { isLocationEnabled } from 'react-native-android-location-enabler';
 import { promptForEnableLocationIfNeeded } from 'react-native-android-location-enabler';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+// import MapView from 'react-native-maps';
+// import UrlTile from 'react-native-maps'
 
 interface Position {
     coords: {
@@ -71,7 +73,8 @@ export default function GeoLoaction() {
                             // Linking.openSettings()
                         }
                         Alert.alert(error.message)
-                    }
+                    },
+                    { enableHighAccuracy: true }
 
                 )
             } else {
@@ -104,6 +107,35 @@ export default function GeoLoaction() {
     //    },[])
     // console.log(currenntLocation)
 
+
+    const openMap = async () => {
+        let url = `geo:0,0?q=${currenntLocation?.coords.latitude},${currenntLocation?.coords.longitude}`
+        await Linking.openURL(url)
+    }
+
+    const [date, setDate] = useState('')
+
+    const convertToDate = () => {
+        let time: Date | string = currenntLocation?.timestamp
+        const date = new Date(time)
+        let readableDate = date.toTimeString()
+        console.log(readableDate)
+        setDate(readableDate)
+    }
+    useEffect(() => {
+        convertToDate()
+    }, [currenntLocation])
+
+    useEffect(() => {
+        const watchId = Geolocation.watchPosition(
+            (position) => {
+                setCurrentLocation(position)
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
+    }, [])
     return (
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Text>GeoLoaction</Text>
@@ -112,17 +144,29 @@ export default function GeoLoaction() {
             </TouchableOpacity>
             {currenntLocation &&
                 <>
-                    <View style={{ backgroundColor: '#bce887', justifyContent: 'center', alignItems: 'center', height: 200, width: '100%', marginTop: 50 }}>
-                        <Text style={styles.text}>latitude:{currenntLocation.coords.latitude}</Text>
+                    <View style={{ backgroundColor: '#bce887', justifyContent: 'center', alignItems: 'center', height: 'auto', width: '100%', marginTop: 50 }}>
+                        <Text style={[styles.text, { marginVertical: 30 }]}>Date & Time:{date}</Text>
+                        <Text style={[styles.text]}>latitude:{currenntLocation.coords.latitude}</Text>
                         <Text style={styles.text}>altitude:{currenntLocation.coords.altitude}</Text>
                         <Text style={styles.text}>longitude:{currenntLocation.coords.longitude}</Text>
                         <Text style={styles.text}>accuracy:{currenntLocation.coords.accuracy}</Text>
                         <Text style={styles.text}>altitudeAccuracy:{currenntLocation.coords.altitudeAccuracy}</Text>
                         <Text style={styles.text}>Speed:{currenntLocation.coords.speed}</Text>
                     </View>
-                    <TouchableOpacity style={{ backgroundColor: 'green', width: width / 2, borderRadius: 10, height: 40,marginTop:20, justifyContent: 'center' }} >
-                        <Text style={{ textAlign: 'center', color: 'white' }} >Open Map</Text>
+                    <TouchableOpacity style={{ backgroundColor: 'green', width: width - 40, borderRadius: 10, height: 50, marginTop: 20, justifyContent: 'center' }} onPress={openMap} >
+                        <Text style={{ textAlign: 'center', color: 'white' }} >View Your current location in Google Map</Text>
                     </TouchableOpacity>
+                    {/* <MapView provider='google'
+                    
+                    initialRegion={{
+                        latitude: currenntLocation.coords.latitude,
+                        longitude: currenntLocation.coords.longitude,
+                        // latitudeDelta:currenntLocation.coords.la,
+                        // longitudeDelta: 0.0421,
+                      }}
+                      /> */}
+
+
                 </>
 
 
