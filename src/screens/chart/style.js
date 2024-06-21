88eeeb0ae790c73005bd296976f307177d973d5f
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-06-21 13:36:40 +0530

    chart--

---
+++ src/screens/chart/style.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+const styles =(width, height)=>
+     StyleSheet.create({
+    container:{
+        flex:1,
+        padding:10
+    }
+    ,
+    subChart:{
+            backgroundColor:'white',
+            // flex:1,
+            borderRadius:15,
+            height:'auto',
+            shadowColor: "#000",
+            marginVertical:10,
+            // shadowOffset: {
+            //     width: 0,
+            //     height: 1,
+            // },
+            // shadowOpacity: 0.44,
+            // shadowRadius: 2.22,
+            
+            elevation: 6,        
+            width : '100%'
+    },
+    subscriberHeading:{
+        color:'black',
+        fontSize:20,
+        fontWeight:'700',
+        margin:10
+
+    },
+   
+})
+
+export default styles
