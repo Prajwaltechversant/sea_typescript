2db94bea5310889ba7484c786674f314f44a6741
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-06-24 18:15:45 +0530

    Merge branch 'ui'

---
+++ src/screens/ui/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+    cardContainer:{
+        width:150,
+        height:150,
+        borderRadius:20,
+        borderWidth:1,
+    }
+})
+
+export default styles
