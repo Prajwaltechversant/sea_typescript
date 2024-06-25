d88c264231651b3a5af50394635705664e0f78f0
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-06-25 10:08:01 +0530

    callender

--- src/screens/data/index.js
+++ src/screens/data/index.js
@@ -1,16 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 import Slider from '@react-native-assets/slider'
 import SliderComponent from '../../components/slider'
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { Alert } from 'react-native';
 
 export default function Data() {
-  return (
-    <View>
-      <Slider
+  const [selected, setSelected] = useState('');
+  console.log(selected)
 
+  return (
+    <View style={{padding:10}}>
+      <ScrollView>
+      <Calendar
+        onDayPress={day => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: { selected: true, disableTouchEvent: true, selectedDotColor: 'orange' }
+        }}
+
+        style={{
+          borderWidth:1,
+          borderRadius:10
+          
+        }}
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+        }}
 
       />
-      <SliderComponent />
+      </ScrollView>
+
+     
+
     </View>
   )
 }
--- src/screens/ui/index.tsx
+++ src/screens/ui/index.tsx
@@ -1,25 +1,38 @@
-import { View, Text, ImageBackground, EasingFunction } from 'react-native'
-import React from 'react'
-import styles from './style'
+import { View, Text, ImageBackground,Button } from 'react-native';
+import React, { useEffect } from 'react';
+import styles from './style';
 import { BlurView } from "@react-native-community/blur";
-import HomeIcon from '../../assets/images/home.svg'
+import HomeIcon from '../../assets/images/home.svg';
+import { Circle, Svg } from "react-native-svg";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Logo from '../../assets/images/logo.svg';
 
-import { Circle, Svg  } from "react-native-svg";
-import Animated, { useSharedValue } from 'react-native-reanimated';
 export default function UiSamples() {
+    const skew = useSharedValue(0);
 
-    const r = useSharedValue(20)
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         skewX.value = withTiming(skewX.value + 30, { duration: 1000 });
+    //     }, 2000);
+
+    //     return () => clearInterval(interval);
+    // }, [skewX]);
 
 
-
-
+    // const btnPress = () =>{
+    //     skewY.value + 30
+    // }
+    // const animatedStyle = useAnimatedStyle(() => {
+    //     return {
+    //         transform: [{ skewX: `0deg` }],
+    //     };
+    // });
 
     return (
         // <ImageBackground
         //     //  source={require('../../assets/images/bg.jpg')} 
         //     // source={require('../../assets/images/bg2.jpg')} 
         //     source={require('../../assets/images/bg3.jpg')}
-
         //     style={styles.container}>
         //     <BlurView
         //         style={styles.cardContainer}
@@ -27,25 +40,21 @@ export default function UiSamples() {
         //         blurAmount={20}
         //         reducedTransparencyFallbackColor="black"
         //         blurRadius={10}
-
         //     >
         //         <Text style={{ textAlign: 'center', fontSize: 20, color: 'black' }}>Hello World</Text>
         //     </BlurView>
-
-        //     <HomeIcon width={30} height={30}  color={'red'} />
-
-
+        //     <HomeIcon width={30} height={30} color={'red'} />
         //     <Text style={{ textAlign: 'center', fontSize: 20, color: 'black', position: 'absolute', transform: [{ translateX: -50, }] }}>Hello World</Text>
-
-
-
         // </ImageBackground>
 
         <View style={styles.container}>
-            <Svg>
-                <Circle x={50} y={50} r={20}  fill={'green'}  />
-            </Svg>
-
+            {/* <Svg>
+                <Circle x={50} y={50} r={20} fill={'green'} />
+            </Svg> */}
+            <Animated.View   style={{transform:[{skewY:'160deg'}]}}>
+                <Logo width={250} height={250} />
+            </Animated.View>
+            {/* <Button title='skew' onPress={btnPress} /> */}
         </View>
-    )
+    );
 }
