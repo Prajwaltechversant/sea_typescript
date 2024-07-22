c435ef23a6eef0fd693751b1b45e4e26648b6e9e
PrajwalTechversant <prajwal.vk@techversantinfotech.com>
2024-07-22 17:53:35 +0530

    On ui: .

---
+++ src/new.tsx
@@ -0,0 +1,11 @@
+// NewComponent.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NewComponent = () => (
+  <View>
+    <Text>New Component</Text>
+  </View>
+);
+
+export default NewComponent;
